7416e007c57ab30f0b0c5ea4e11613837e9da83b
Michael Xia <mxia.mit@gmail.com>
2016-03-20 11:41:10 -0700

    config added

---
+++ config.js
@@ -0,0 +1,11 @@
+var config = {};
+
+config.port = 1234;
+config.message = 'Hi! To reach us, email hi.honey.bunches@gmail.com!'
+config.numbers = {
+    '+14158552737' : 'NEW',
+    '+13023546886' : 'Mike Teng'
+};
+config.smtps = 'smtps://hihoneybunches%40gmail.com:marvinminsky@smtp.gmail.com'
+
+module.exports = config;
--- index.js
+++ index.js
@@ -3,27 +3,31 @@ var http = require('http'),
     twilio = require('twilio'),
     bodyParser = require('body-parser'),
     nodemailer = require('nodemailer'),
-    _ = require('lodash')
+    _ = require('lodash'),
+    config = require('./config')
 
-var transporter = nodemailer.createTransport('smtps://hihoneybunches%40gmail.com:marvinminsky@smtp.gmail.com');
+var transporter = nodemailer.createTransport(config.smtps);
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/', function(req, res) {
+
+    var name = config.numbers[req.body.To] || 'NumberNotInConfig'
+
     transporter.sendMail({
-        from: '"TWILIO NUMBER" <hi.honey.bunches@gmail.com>',
+        from: '"Airbnb Twilio Bot" <hi.honey.bunches@gmail.com>',
         to: 'mxia.mit@gmail.com, csample@cca.edu, andrew.h.jiang@gmail.com',
-        subject: 'SMS RECEIVED',
-        text: req.body.From + ' sent "' + req.body.Body + '" to ' + req.body.To
+        subject: 'Airbnb SMS',
+        text: req.body.From + ' sent "' + req.body.Body + '" to ' + req.body.To + ' under ' + name
     });
 
     var twiml = new twilio.TwimlResponse();
-    twiml.message('Hi! To reach us, email hi.honey.bunches@gmail.com!');
+    twiml.message(config.message);
     res.writeHead(200, {'Content-Type': 'text/xml'});
     res.end(twiml.toString());
 });
 
-http.createServer(app).listen(1234, function () {
-    console.log("Express server listening on port 1234");
+http.createServer(app).listen(config.port, function () {
+    console.log("Express server listening on port" + config.port);
 });
