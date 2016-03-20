05073adf73d0a59f18929ddf098a97fec921cf10
Michael Xia <mxia.mit@gmail.com>
2016-03-20 11:27:04 -0700

    save

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
-var twilio = require('twilio')
-var nodemailer = require('nodemailer')
-var _ = require('lodash')
-var http = require('http')
+var http = require('http'),
+    express = require('express'),
+    twilio = require('twilio'),
+    bodyParser = require('body-parser'),
+    nodemailer = require('nodemailer'),
+    _ = require('lodash')
 
-var transporter = nodemailer.createTransport('smtps://hihoneybunches%40gmail.com:marvinminsky@smtp.gmail.com');
+// var transporter = nodemailer.createTransport('smtps://hihoneybunches%40gmail.com:marvinminsky@smtp.gmail.com');
 
-var mailOptions = {
-    from: '"TWILIO NUMBER" <hi.honey.bunches@gmail.com>',
-    to: 'mxia.mit@gmail.com, csample@cca.edu, andrew.h.jiang@gmail.com',
-    subject: 'SMS RECEIVED',
-    text: ''
-};
+// var mailOptions = {
+//     from: '"TWILIO NUMBER" <hi.honey.bunches@gmail.com>',
+//     to: 'mxia.mit@gmail.com, csample@cca.edu, andrew.h.jiang@gmail.com',
+//     subject: 'SMS RECEIVED',
+//     text: ''
+// };
 
-http.createServer(function (req, res) {
-    transporter.sendMail(_.assign(mailOptions, { text: JSON.stringify(req) }), function(error, info){
-        res.writeHead(200, {'Content-Type': 'text'});
-        res.end("OK")
-    });
-}).listen(1234);
+// http.createServer(function (req, res) {
+//     transporter.sendMail(_.assign(mailOptions, { text: JSON.stringify(req) }), function(error, info){
+//         res.writeHead(200, {'Content-Type': 'text'});
+//         res.end("OK")
+//     });
+// }).listen(1234);
 
-console.log('twilio listening on 1234')
+// console.log('twilio listening on 1234')
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/', function(req, res) {
+    console.log(req.body)
+    // var twiml = new twilio.TwimlResponse();
+    // if (req.body.Body == 'hello') {
+    //     twiml.message('Hi!');
+    // } else if(req.body.Body == 'bye') {
+    //     twiml.message('Goodbye');
+    // } else {
+    //     twiml.message('No Body param match, Twilio sends this in the request to your server.');
+    // }
+    // res.writeHead(200, {'Content-Type': 'text/xml'});
+    // res.end(twiml.toString());
+});
+
+http.createServer(app).listen(1234, function () {
+    console.log("Express server listening on port 1234");
+});
